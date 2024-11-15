b88ce88f82cec005eb43a4e56c8d1d46887a9172
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-11-15 15:32:36 +0100

    ended snack 7 with find()

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,10 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+const studentId2 = students.find(member => {
+  if (member.id === 2) {
+    return member;
+  }
+});
+console.log(studentId2);
